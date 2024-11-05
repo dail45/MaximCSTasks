175d980039dff8219bef5d9415f035b80e127dd7
dail45 <dail45@yandex.ru>
2024-11-05 17:21:05 +0500

    Task2 Solved

--- MaximCSTasks/Program.cs
+++ MaximCSTasks/Program.cs
@@ -3,10 +3,19 @@
 Console.Write("Enter your line: ");
 var line = Console.ReadLine();
 
-if (string.IsNullOrEmpty(line)) {
+if (string.IsNullOrEmpty(line))
+{
     Console.WriteLine("Your line is null");
     Environment.Exit(0);
 }
 
-var result = Utils.EvenOrOddReverseTextFunc(line);
-Console.WriteLine($"Result: \"{line}\" -> \"{result}\"");
+var unexpectedChars = Utils.CheckOnlyEnglishChars(line);
+if (unexpectedChars.Count > 0)
+{
+    Console.WriteLine("Your line contains unexpected characters: " + string.Join(", ", unexpectedChars.Select(ch => $"\"{ch}\"")));
+}
+else
+{
+    var result = Utils.EvenOrOddReverseTextFunc(line);
+    Console.WriteLine($"Result: \"{line}\" -> \"{result}\"");
+}
--- MaximCSTasks/Utils.cs
+++ MaximCSTasks/Utils.cs
@@ -2,12 +2,27 @@
 
 public class Utils
 {
-    public static string EvenOrOddReverseTextFunc(string text) {
-        if (text.Length % 2 == 0) {
-            int halfTextIndex = text.Length / 2;
+    public static string EvenOrOddReverseTextFunc(string text)
+    {
+        if (text.Length % 2 == 0)
+        {
+            var halfTextIndex = text.Length / 2;
 
             return new string(text[..halfTextIndex].Reverse().Concat(text[halfTextIndex..].Reverse()).ToArray());
         }
+
         return new string(text.Reverse().Concat(text).ToArray());
+    }
+
+    public static List<char> CheckOnlyEnglishChars(string text)
+    {
+        var unexpectedChars = new List<char>();
+        foreach (var ch in text)
+        {
+            if ('a' > ch || ch > 'z')
+                unexpectedChars.Add(ch);
+        }
+
+        return unexpectedChars;
     }
 }
