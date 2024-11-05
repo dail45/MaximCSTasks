365ff4fa3d63f66967cee41d4a5e87b4a8260bcb
dail45 <dail45@yandex.ru>
2024-11-05 20:16:45 +0500

    Task5 Solved

--- MaximCSTasks/Program.cs
+++ MaximCSTasks/Program.cs
@@ -1,27 +1,42 @@
-﻿using MaximCSTasks;
+﻿using System.Diagnostics;
+using MaximCSTasks;
 using MaximCSTasks.Sorters;
 
 Console.Write("Enter your line: ");
-var line = Console.ReadLine();
+var inputLine = Console.ReadLine();
 
-if (string.IsNullOrEmpty(line))
+if (string.IsNullOrEmpty(inputLine))
 {
     Console.WriteLine("Your line is null");
     Environment.Exit(0);
 }
 
-var unexpectedChars = Utils.CheckOnlyEnglishChars(line);
+Console.Write("Choose sorter for your line (q - QuickSorter, t - TreeSorter): ");
+var sorterLine = Console.ReadLine();
+if (string.IsNullOrEmpty(sorterLine) || sorterLine.Length > 1 || !"qt".Contains(sorterLine[0]))
+{
+    Console.WriteLine("Your choose is invalid");
+    Environment.Exit(0);
+}
+
+StringSorterInterface sorter = sorterLine switch
+{
+    "q" => new StringQuickSorter(),
+    "t" => new StringTreeSorter()
+};
+
+var unexpectedChars = Utils.CheckOnlyEnglishChars(inputLine);
 if (unexpectedChars.Count > 0)
 {
     Console.WriteLine("Your line contains unexpected characters: " + string.Join(", ", unexpectedChars.Select(ch => $"\"{ch}\"")));
 }
 else
 {
-    var result = Utils.EvenOrOddReverseTextFunc(line);
+    var result = Utils.EvenOrOddReverseTextFunc(inputLine);
     var charsCount = Utils.CalcCountChars(result);
     var vowelLargestSubString = Utils.FindMaxLengthSubStringOfVowelChars(result);
-    var sortedResultLine = new StringTreeSorter().SortString(result);
-    Console.WriteLine($"Result: \"{line}\" -> \"{result}\"");
+    var sortedResultLine = sorter.SortString(result);
+    Console.WriteLine($"Result: \"{inputLine}\" -> \"{result}\"");
     Console.WriteLine("Counts of chars in result line:");
     Console.WriteLine(string.Join("\n", charsCount.Select(pair => $"\"{pair.Key}\": {pair.Value}")));
     Console.WriteLine($"Largest vowel substring: \"{vowelLargestSubString}\"");
