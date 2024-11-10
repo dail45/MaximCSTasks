b645ec2ba490787a5317d7c8232298d56186ff61
dail45 <dail45@yandex.ru>
2024-11-10 17:23:47 +0500

    --RandomNumberGeneratorService

---
+++ MaximCSTasks/Services/RandomNumberGeneratorService.cs
@@ -0,0 +1,33 @@
+﻿using MaximCSTasks.RandomNumberGenerator;
+
+namespace MaximCSTasks.Services;
+
+public class RandomNumberGeneratorService
+{
+    private static readonly RandomNumberGeneratorService Instance;
+
+    static RandomNumberGeneratorService()
+    {
+        Instance = new RandomNumberGeneratorService();
+    }
+    
+    private RandomNumberGeneratorService() { }
+    
+    public static RandomNumberGeneratorService GetInstance => Instance;
+    
+
+    private readonly RemoteRandomNumberGenerator _remoteRandomNumberGenerator = new RemoteRandomNumberGenerator();
+    private readonly LocalRandomNumberGenerator _localRandomNumberGenerator = new LocalRandomNumberGenerator();
+    
+    public int GetRandomNumber(int min, int max)
+    {
+        try
+        {
+            return _remoteRandomNumberGenerator.GetRandomNumber(min, max);
+        }
+        catch
+        {
+            return _localRandomNumberGenerator.GetRandomNumber(min, max);
+        }
+    }
+}
